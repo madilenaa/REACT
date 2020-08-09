15604a9c57194f0b9ff2c3342bf6134179f76a65
Magdalena Marchewka <madilenaa@gmail.com>
2020-08-09 13:40:21 +0200

    add project

---
+++ react-learning-master/react-starter-fer-11/src/components/Hero/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styles from './Hero.scss';
+import PropTypes from 'prop-types';
+import ReactHtmlParser from 'react-html-parser';
+
+const Hero = props => (
+  <header className={styles.component}>
+    <h2 className={styles.title}>{ReactHtmlParser(props.titleText)}</h2>
+    <img className={styles.image} src={props.image}></img>
+  </header>
+);
+Hero.propTypes = {
+titleText: PropTypes.node,
+};
+
+export default Hero;
