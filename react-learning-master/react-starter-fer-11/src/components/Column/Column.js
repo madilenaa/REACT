48c48d4dc51c76fcafa6bd6c5dd84c60b0a84a34
Magdalena Marchewka <madilenaa@gmail.com>
2020-09-03 20:23:41 +0200

    add Column and Cards

--- react-learning-master/react-starter-fer-11/src/components/Column/Column.js
+++ react-learning-master/react-starter-fer-11/src/components/Column/Column.js
@@ -2,8 +2,8 @@ import React from 'react';
 import styles from './Column.scss';
 import PropTypes from 'prop-types';
 import Card from '../Card/Card.js';
-//import {settings} from '../../data/dataStore';
-//import Creator from '../Creator/Creator.js';
+import {settings} from '../../data/dataStore';
+import Creator from '../Creator/Creator.js';
 import Icon from '../Icon/Icon.js';
 
 class Column extends React.Component {
@@ -11,15 +11,21 @@ class Column extends React.Component {
     title: PropTypes.string.isRequired,
     cards: PropTypes.array,
     icon: PropTypes.node,
+    addCard: PropTypes.func,
   }
+
+  static defaultProps = {
+    icon: settings.defaultColumnIcon,
+  }
+
   render () {
-    const {title, icon, cards} = this.props;
+    const {title, icon, cards, addCard} = this.props;
     return (
       <section className={styles.component}>
         <h3 className={styles.title}><span className={styles.icon}><Icon name={icon} /></span>{title}</h3>
-        {/*}<div className={styles.creator}>
-          <Creator text={settings.cardCreatorText} action={title => this.addCard(title)} />
-        </div> */}
+        <div className={styles.creator}>
+          <Creator text={settings.cardCreatorText} action={addCard} />
+        </div>
         <div className={styles.cards}>
           {cards.map(cardsData => (
             <Card key={cardsData.id} {...cardsData}/>
--- react-learning-master/react-starter-fer-11/src/components/List/ListContainer.js
+++ react-learning-master/react-starter-fer-11/src/components/List/ListContainer.js
@@ -1,10 +1,16 @@
 import {connect} from 'react-redux';
 import List from './List';
+import {getColumnsForList, createActionAddColumn} from '../../redux/columnsRedux.js';
 
 const mapStateToProps = (state, props) => ({
   columns: getColumnsForList(state, props.id),
 });
 
-export const getColumnsForList = ({columns}, listId) => columns.filter(column => column.listId == listId);
+const mapDispatchToProps = (dispatch, props) => ({
+  addColumn: title => dispatch(createActionAddColumn({
+    listId: props.id,
+    title,
+  })),
+});
 
-export default connect(mapStateToProps)(List);
+export default connect(mapStateToProps, mapDispatchToProps)(List);
